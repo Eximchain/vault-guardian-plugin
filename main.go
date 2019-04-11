0b1d82c04ec472093d99d3fbc98406dbc7bfe756
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-04-11 12:36:25 -0400

    Logging in main function

--- main.go
+++ main.go
@@ -10,18 +10,26 @@ import (
 )
 
 func main() {
+	log.Println("PLUGIN TEST 1")
 	apiClientMeta := &pluginutil.APIClientMeta{}
+	log.Println("PLUGIN TEST 2")
 	flags := apiClientMeta.FlagSet()
+	log.Println("PLUGIN TEST 3")
 	flags.Parse(os.Args[1:]) // Ignore command, strictly parse flags
+	log.Println("PLUGIN TEST 4")
 
 	tlsConfig := apiClientMeta.GetTLSConfig()
+	log.Println("PLUGIN TEST 5")
 	tlsProviderFunc := pluginutil.VaultPluginTLSProvider(tlsConfig)
+	log.Println("PLUGIN TEST 6")
 
 	err := plugin.Serve(&plugin.ServeOpts{
 		BackendFactoryFunc: guardian.Factory,
 		TLSProviderFunc:    tlsProviderFunc,
 	})
+	log.Println("PLUGIN TEST 7")
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("PLUGIN TEST 8")
 }
