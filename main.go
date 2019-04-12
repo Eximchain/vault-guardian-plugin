692ce50eb59b3b3fcfd634e3917566118ed4fae8
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-04-12 11:57:46 -0400

    Log TLS Config

--- main.go
+++ main.go
@@ -12,26 +12,19 @@ import (
 
 func main() {
 	hclog.Default().Info("HCLOG PLUGIN TEST MAIN")
-	log.Println("PLUGIN TEST 1")
 	apiClientMeta := &pluginutil.APIClientMeta{}
-	log.Println("PLUGIN TEST 2")
 	flags := apiClientMeta.FlagSet()
-	log.Println("PLUGIN TEST 3")
 	flags.Parse(os.Args[1:]) // Ignore command, strictly parse flags
-	log.Println("PLUGIN TEST 4")
 
 	tlsConfig := apiClientMeta.GetTLSConfig()
-	log.Println("PLUGIN TEST 5")
 	tlsProviderFunc := pluginutil.VaultPluginTLSProvider(tlsConfig)
-	log.Println("PLUGIN TEST 6")
+	hclog.Default().Info("TLS CONFIG:", tlsConfig)
 
 	err := plugin.Serve(&plugin.ServeOpts{
 		BackendFactoryFunc: guardian.Factory,
 		TLSProviderFunc:    tlsProviderFunc,
 	})
-	log.Println("PLUGIN TEST 7")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("PLUGIN TEST 8")
 }
